03403ab1d8889999404ee71c23a98257b1c12ce4
wldusdn <wldusdngg@naver.com>
2025-04-25 14:52:12 +0900

    :construction: wip: 시설 리스트 조회 폼 생성 중

--- src/views/places/AppPlace.vue
+++ src/views/places/AppPlace.vue
@@ -2,225 +2,86 @@
   <div>
     <!-- Breadcrumb -->
     <Breadcrumb breadcrumb="place" />
-
-    <div class="mt-5">
-      <div class="flex flex-col mt-3 sm:flex-row">
-          <div class="flex">
-            <div class="relative">
-              <select
-                class="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white border border-gray-400 rounded-l appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
-              >
-                <option>All</option>
-                <option>공지사항</option>
-                <option>자유게시판</option>
-                <option>이벤트</option>
-                <option>케어시트</option>
-              </select>
-
-              <div
-                class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none"
-              >
-                <svg
-                  class="w-4 h-4 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <div class="relative">
-              <select
-                class="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white border-t border-b border-r border-gray-400 rounded-r appearance-none sm:rounded-r-none sm:border-r-0 focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500"
-              >
-                <option>All</option>
-                <option>관리자</option>
-                <option>회원</option>
-              </select>
-
-              <div
-                class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none"
-              >
-                <svg
-                  class="w-4 h-4 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div>
-
-          <div class="relative block mt-2 sm:mt-0">
-            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-              <svg
-                viewBox="0 0 24 24"
-                class="w-4 h-4 text-gray-500 fill-current"
-              >
-                <path
-                  d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"
-                />
-              </svg>
-            </span>
-
-            <input
-              placeholder="Search"
-              class="block w-full py-2 pl-8 pr-6 text-sm text-gray-700 placeholder-gray-400 bg-white border border-b border-gray-400 rounded-l rounded-r appearance-none sm:rounded-l-none focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
-            />
-          </div>
-        </div>
-
-      <div class="flex flex-col mt-6">
+    <div class="mt-4 mb-3">
+      <div class="flex flex-wrap gap-4">
         <div
-          class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8"
-        >
-          <div
-            class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg"
-          >
-            <table class="min-w-full">
-              <thead>
-                <tr>
-                  <th
-                    class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase bg-gray-100 border-b border-gray-200"
-                  >
-                    Title
-                  </th>
-                  <th
-                    class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase bg-gray-100 border-b border-gray-200"
-                  >
-                    Name
-                  </th>
-                  <th
-                    class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase bg-gray-100 border-b border-gray-200"
-                  >
-                    view
-                  </th>
-                  <th
-                    class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase bg-gray-100 border-b border-gray-200"
-                  >
-                    Comments
-                  </th>
-                  <th
-                    class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase bg-gray-100 border-b border-gray-200"
-                  >
-                    Created at
-                  </th>
-                  <th
-                    class="px-6 py-3 bg-gray-100 border-b border-gray-200"
-                  ></th>
-                </tr>
-              </thead>
-
-              <tbody class="bg-white">
-                <tr
-                  v-for="u in wideTableData"
-                  :key="u.id"
-                  class="cursor-pointer hover:bg-gray-100"
-                  @click="goToDetail(u.id)"
-                >
-                  <td
-                    class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
-                  >
-                      <div
-                        class="text-sm font-medium leading-5 text-gray-900"
-                      >
-                        {{ u.title }}
-                      </div>
-                  </td>
-
-                  <td
-                    class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
-                  >
-                    <div class="text-sm leading-5 text-gray-900">
-                      {{ u.name }}
-                    </div>
-                  </td>
-
-                  <td
-                    class="px-6 py-4 text-sm leading-5 text-gray-500 border-b border-gray-200 whitespace-nowrap"
-                  >
-                    {{ u.count }}
-                  </td>
-
-                  <td
-                    class="px-6 py-4 text-sm leading-5 text-gray-500 border-b border-gray-200 whitespace-nowrap"
-                  >
-                    {{ u.comments }}
-                  </td>
-
-                  <td
-                    class="px-6 py-4 text-sm leading-5 text-gray-500 border-b border-gray-200 whitespace-nowrap"
-                  >
-                    {{ u.createdAt }}
-                  </td>
-
-                  <td
-                    class="px-6 py-4 text-sm font-medium leading-5 text-right border-b border-gray-200 whitespace-nowrap"
-                  >
-                    <div class="flex justify-around">
-                      <span class="text-yellow-500 flex justify-center">
-                        <a href="#" class="mx-2 px-2 rounded-md"
-                          ><svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="h-5 w-5 text-green-700"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
-                            />
-                            <path
-                              fill-rule="evenodd"
-                              d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                              clip-rule="evenodd"
-                            />
-                          </svg>
-                        </a>
-                        <form method="POST">
-                          <button class="mx-2 px-2 rounded-md">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="h-5 w-5 text-red-700"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
-                          </button>
-                        </form>
-                      </span>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          v-for="u in placeTableData"
+          :key="u.id"
+          class="max-w-sm mt-6 overflow-hidden bg-white rounded shadow-lg">
+          <img
+            class="w-full"
+            src="https://picsum.photos/id/1016/384/234"
+            alt="Sunset in the mountains"
+          />
+          <div class="px-6 py-4">
+            <div class="mb-2 text-xl font-bold text-gray-900">
+              {{ u.placeName }}
+            </div>
+            <div class="text-base text-gray-700">
+              <div class="flex gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" class="w-1/12" aria-hidden="true">
+                  <path d="M9,1C5.4,1,2.5,3.7,2.5,7.1c0,1.2,0.4,2.3,1,3.3l5.1,6.1c0.2,0.2,0.6,0.2,0.8,0l5.1-6.1c0.7-1,1-2.1,1-3.3C15.5,3.7,12.6,1,9,1z M9,9C8.2,9,7.5,8.3,7.5,7.5C7.5,6.7,8.2,6,9,6s1.5,0.7,1.5,1.5C10.5,8.3,9.8,9,9,9z" fill="gray"></path>
+                </svg>
+                {{ u.address }}
+              </div>
+              <div class="flex items-start gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" class="w-1/12" aria-hidden="true">
+                  <path d="M9,1.5C4.9,1.5,1.5,4.9,1.5,9c0,4.1,3.4,7.5,7.5,7.5c4.1,0,7.5-3.4,7.5-7.5C16.5,4.9,13.1,1.5,9,1.5zM11.7,11.5c-0.2,0.3-0.6,0.3-0.9,0.1L8.4,9.9V5.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v3.4l1.9,1.4C11.8,10.8,11.9,11.2,11.7,11.5z" fill="gray"></path>
+                </svg>
+                <div>
+                  <p v-for="(hour, index) in u.hours" :key="index">
+                    {{ getFormattedHours(hour) }}<br v-if="index < u.hours.length - 1" />
+                  </p>
+                </div>
+              </div>
+              <div class="flex gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" class="w-1/12" aria-hidden="true">
+                  <path d="M13.8,15.6c-0.2,0.2-0.6,0.3-0.9,0.3C7.6,14.6,3.4,10.4,2.1,5.1C2,4.8,2.1,4.5,2.4,4.2l1.5-1.5c0.4-0.4,1-0.4,1.4,0l0,0l2.5,2.5c0.4,0.4,0.4,1,0,1.4L7.8,6.7L7.1,7.5C6.7,7.8,6.7,8.3,6.9,8.7c0.8,1.2,1.9,2.2,3.1,2.9c0,0,0.8-0.7,1.3-1.3c0.4-0.4,1-0.4,1.4,0l2.5,2.5l0,0c0.4,0.4,0.4,1,0,1.4L13.8,15.6z" fill="gray"></path>
+                </svg>
+                {{ u.contact }}
+              </div>
+            </div>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span
+              class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+              >{{ u.category }}</span
+            >
+            <span
+              class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+              >별점 {{ u.score }}</span
+            >
+            <span
+              class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+              >{{ u.type }}</span
+            >
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import { useTableData } from '../../hooks/useTableData'
-const { wideTableData } = useTableData()
+<script lang="ts" setup>
 import Breadcrumb from '../../partials/AppBreadcrumb.vue'
-import { useRouter } from 'vue-router';
+import { useTableData } from '../../hooks/useTableData'
+const { placeTableData } = useTableData()
 
-const router = useRouter();
-
-const goToDetail = (id: number) => {
-  router.push(`/board/${id}`);
+interface Hour {
+  day: string;
+  start: string;
+  end: string;
 }
 
+const getFormattedHours = (hour: Hour): string => {
+  const dayMap: { [key: string]: string } = {
+    monday: '월',
+    tuesday: '화',
+    wednesday: '수',
+    thursday: '목',
+    friday: '금',
+    saturday: '토',
+    sunday: '일',
+  };
+  return `${dayMap[hour.day]}: ${hour.start}-${hour.end}`;
+};
 </script>
