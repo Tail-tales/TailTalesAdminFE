28d0fc1008077c3370fbda0fcf49b78b6fa2ee39
wldusdn <wldusdngg@naver.com>
2025-04-30 14:21:22 +0900

    :lipstick: style: 시설리스트에 수정, 삭제버튼 추가

--- src/views/places/AppPlace.vue
+++ src/views/places/AppPlace.vue
@@ -23,9 +23,9 @@
             alt="Sunset in the mountains"
           />
           <div class="px-6 py-4">
-            <div class="mb-2 text-xl font-bold text-gray-900">
-              {{ u.placeName }}
-            </div>
+              <div class="mb-2 text-xl font-bold text-gray-900">
+                {{ u.placeName }}
+              </div>
             <div class="text-base text-gray-700">
               <div class="flex gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" class="w-1/12" aria-hidden="true">
@@ -51,19 +51,54 @@
               </div>
             </div>
           </div>
-          <div class="px-6 pt-4 pb-2">
-            <span
-              class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
-              >{{ u.category }}</span
-            >
-            <span
-              class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
-              >별점 {{ u.score }}</span
-            >
-            <span
-              class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
-              >{{ u.type }}</span
-            >
+          <div class="flex justify-between items-center">
+            <div class="px-6 pt-4 pb-2">
+              <span
+                class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+                >{{ u.category }}</span
+              >
+              <span
+                class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+                >별점 {{ u.score }}</span
+              >
+              <span
+                class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full"
+                >{{ u.type }}</span
+              >
+            </div>
+            <div class="mx-4">
+              <button class="mr-4 rounded-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 text-green-700"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
+              <button class="rounded-md">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 text-red-700"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button> 
+            </div>
           </div>
         </div>
       </div>
